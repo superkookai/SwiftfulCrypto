e9fb9c54dcfe75cdedcd116106938a9244cc7545
Weerawut Chaiyasomboon <weerawut@live.com>
2023-12-06 13:30:47 +0700

    Setting up project.

---
+++ SwiftfulCrypto/Extensions/Color.swift
@@ -0,0 +1,20 @@
+//
+//  Color.swift
+//  SwiftfulCrypto
+//
+//  Created by Weerawut Chaiyasomboon on 6/12/2566 BE.
+//
+
+import SwiftUI
+
+extension Color{
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme{
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
