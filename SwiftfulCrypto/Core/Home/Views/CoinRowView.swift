184b4030108e669b7e0e2046160a312b575a52b8
Weerawut Chaiyasomboon <weerawut@live.com>
2023-12-11 13:25:23 +0700

    Add CoinRowView with NumberFormatter (extension to Double).

---
+++ SwiftfulCrypto/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,76 @@
+//
+//  CoinRowView.swift
+//  SwiftfulCrypto
+//
+//  Created by Weerawut Chaiyasomboon on 11/12/2566 BE.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    let coin: CoinModel
+    let showHoldingColumn: Bool
+    
+    var body: some View {
+        HStack(spacing: 0, content: {
+            leftColumn
+            
+            Spacer()
+            
+            if showHoldingColumn{
+               centerColumn
+            }
+            
+            rightColumn
+            
+        })
+        .font(.subheadline)
+    }
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+    CoinRowView(coin: DeveloperPreview.instance.coin, showHoldingColumn: true)
+}
+
+extension CoinRowView{
+    private var leftColumn: some View{
+        HStack(spacing: 0){
+            Text("\(coin.rank)")
+                .font(.caption)
+                .foregroundStyle(Color.theme.secondaryText)
+                .frame(minWidth: 30)
+            
+            Circle()
+                .frame(width: 30, height: 30)
+            
+            Text(coin.symbol.uppercased())
+                .font(.headline)
+                .foregroundStyle(Color.theme.accent)
+                .padding(.leading, 6)
+        }
+    }
+    
+    private var centerColumn: some View{
+        VStack(alignment: .trailing){
+            Text(coin.currentHoldingsValue.asCurrencyWith2Decimals())
+                .bold()
+            Text((coin.currentHoldings ?? 0).asNumberString())
+        }
+        .foregroundStyle(Color.theme.accent)
+    }
+    
+    private var rightColumn: some View{
+        VStack(alignment: .trailing){
+            Text(coin.currentPrice.asCurrencyWith6Decimals())
+                .bold()
+                .foregroundStyle(Color.theme.accent)
+            Text(coin.priceChangePercentage24H?.asPercentString() ?? "")
+                .foregroundStyle(
+                    (coin.priceChangePercentage24H ?? 0) >= 0 ?
+                    Color.theme.green :
+                        Color.theme.red
+                )
+        }
+        .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
+    }
+}
